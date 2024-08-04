f11f388200736c2fdb06f8d42223dd29562cf405
adhirajGadade1 <adhirajgadade98@gmail.com>
2024-08-04 23:17:05 +0530

    javascriptvaribles
    javascriptvaribles

---
+++ 01-variable.js
@@ -0,0 +1,6 @@
+var fristNAme = "Adhiraj";
+console.log(fristNAme);
+
+var lastName;
+lastName = "Gadade";
+console.log(lastName);
---
+++ 01-variableAssignment.js
@@ -0,0 +1,5 @@
+ console.log("My City NAme Is Sangli"); 
+ console.log("My Best Friend Name is Snehal");
+ console.log("My Country NAme is India");
+ console.log("My School Name is Sangli Highschool Sangli");
+ console.log("My Collage Name is PVPIT");
