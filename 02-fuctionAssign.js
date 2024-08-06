d9812410c2dd253faacfd62cc1c51583e861b6f6
adhirajGadade1 <adhirajgadade98@gmail.com>
2024-08-07 00:48:46 +0530

    function  assignment
    function  assignment

---
+++ 02-fuctionAssign.js
@@ -0,0 +1,24 @@
+function demo(params) {
+    console.log("My Love is JS");
+}
+
+function demtesto(params) {
+    console.log("Coding is my passion");
+}
+
+function personalDetails(fristName, lastName, collageName) {
+    console.log("My Frist Name is:",fristName);
+    console.log("My Last Name is:", lastName );
+    console.log("My Collage Name is:", collageName );
+}
+personalDetails("Adhiraj", "Gadade", "PVPIT");
+
+function addThreeValues(params1, params2, params3) {
+
+    var res = params1 + params2 + params3;
+    console.log(res);
+    
+}
+
+addThreeValues(10.23,600,40);
+addThreeValues("Hello ","Good ","Morning");
---
+++ 03-functionAssign.js
@@ -0,0 +1,9 @@
+function bankDetails(bankName, accountNum, location, pinCode){
+
+console.log(" Bank Details:", bankName, accountNum, location, pinCode);
+
+}
+
+bankDetails("CITI Bank","3458685245", "Pune", "411202");
+bankDetails("Axis bank", "7856762345","Mumbai",441203);
+bankDetails("HDFC Bank", "89754123598","Pune", "411021","Open");
